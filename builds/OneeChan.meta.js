6ba4492059fcc5900fc7b20138930a5370b2dcda
Kevin Parnell <kpbballer@gmail.com>
2018-11-26 00:51:14 -0500

    Update to 5.7.6
    New mascots and theme

--- builds/OneeChan.meta.js
+++ builds/OneeChan.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name OneeChan
-// @version 5.7.5
+// @version 5.7.6
 // @namespace OneeChan
 // @description Customizable rice and themes for 4chan X.
 // @author Kevin Parnell
