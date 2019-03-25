a026d20ccda191fc6cb390323ea9762c5ea8487a
Kevin Parnell <kpbballer@gmail.com>
2019-03-25 14:16:59 -0400

    change version number

--- builds/OneeChan.meta.js
+++ builds/OneeChan.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name OneeChan
-// @version 5.7.7
+// @version 5.7.8
 // @namespace OneeChan
 // @description Customizable rice and themes for 4chan X.
 // @author Kevin Parnell
