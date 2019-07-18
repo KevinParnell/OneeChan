2296cf3b409b490985f08cbac241cf5ab20e5331
Gitea <gitea@fake.local>
2019-07-18 02:47:19 -0400

    forgot one version number

--- builds/OneeChan.user.js
+++ builds/OneeChan.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name OneeChan
-// @version 5.8.7
+// @version 5.8.8
 // @namespace OneeChan
 // @description Customizable rice and themes for 4chan X.
 // @author Kevin Parnell
