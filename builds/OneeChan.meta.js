26aff0b5e1ee17bbae1b99f80fe500d8cc179c6e
Kevin Parnell <kpbballer@gmail.com>
2020-01-21 16:36:18 -0500

    Update OneeChan.meta.js

--- builds/OneeChan.meta.js
+++ builds/OneeChan.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name OneeChan
-// @version 5.9.0
+// @version 5.9.1
 // @namespace OneeChan
 // @description Customizable rice and themes for 4chan X.
 // @author Kevin Parnell
