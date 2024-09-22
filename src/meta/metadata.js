6c2c6095b9bc42d11e33631e8f7899103ba56239
SystemPatch <SystemPatch@users.noreply.github.com>
2024-09-22 02:34:39 -0400

    Move changes from user.js to src
    Generates a roughly 1:1 build of 5.9.3

--- src/meta/metadata.js
+++ src/meta/metadata.js
@@ -1,25 +1,23 @@
-// ==UserScript==
-// @name         <%= meta.name %>
-// @version      <%= version %>
-// @namespace    <%= name %>
-// @description  <%= description %>
-// @minGMVer     <%= meta.min.greasemonkey %>
-// @minFFVer     <%= meta.min.firefox %>
-// @license      <%= license %>; <%= meta.repo %>blob/<%= meta.mainBranch %>/LICENSE 
+// @name <%= meta.name %>
+// @version <%= version %>
+// @namespace <%= name %>
+// @description <%= description %>
+// @author <%= meta.author %>
+// @minGMVer <%= meta.min.greasemonkey %>
+// @minFFVer <%= meta.min.firefox %>
+// @license <%= license %>; <%= meta.repo %>blob/<%= meta.mainBranch %>/LICENSE
 <%=
 meta.matches.map(function(match) {
-return '// @match        ' + match;
+return '// @match ' + match;
 }).join('\n')
 %>
-// @match *://boards.4chan.org/*
-// @match *://boards.4channel.org/*
 // @grant GM_getValue
 // @grant GM_setValue
 // @grant GM_deleteValue
 // @grant GM_openInTab
 // @grant GM_listValues
 // @run-at document-start
-// @updateURL    <%= meta.downloads %><%= meta.files.metajs %>
-// @downloadURL  <%= meta.downloads %><%= meta.files.userjs %>
+// @updateURL <%= meta.downloads %><%= meta.files.metajs %>
+// @downloadURL <%= meta.downloads %><%= meta.files.userjs %>
 // @icon         data:image/png;base64,<%= grunt.file.read('src/img/icon48.png', {encoding: 'base64'}) %>
 // ==/UserScript==
